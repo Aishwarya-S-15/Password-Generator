84ba932033d776a04adb49f718a679a78a97d050
Aishwarya S <aishuyashu1234@gmail.com>
2025-09-16 21:12:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const pass = document.getElementById("password");
+
+const lenght = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_-+={}[]|~`<>/?";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function generate(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    pass.value = password;
+}
+
+function copy(){
+    pass.select();
+    document.execCommand("copy");
+}
